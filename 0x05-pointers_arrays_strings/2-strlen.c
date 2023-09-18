f90b475bc9174d27875680a2eb9b9bb5e831a99d
ahmed saad <ahmed.mail10.as@gmail.com>
2023-09-18 16:22:57 -0700

    s

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,13 @@
  *
  * return: length of string
  */
+
 int _strlen(char *s)
 {
 	int counter;
 
-	for (counter = 0; *s != '\0';s++)
-	       ++counter;
+	for (counter = 0; *s != '\0'; s++)
+	 ++counter;
 
 	return (counter);
 }
